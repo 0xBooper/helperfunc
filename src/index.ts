df3e7bc387542dacb96dfaa1a483ebac3eb36fc7
0xBooper <77999320+0xBooper@users.noreply.github.com>
2021-07-22 21:45:15 -0500

    fix: dynamic exports not working

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 // Import all the functions
 import randomBool from "./functions/randomBool"
 
-// Make a object with all the functions
-const functions = {randomBool}
-
 // Export it!
-export default functions
+export {
+    randomBool
+} 
