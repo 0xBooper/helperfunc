8f671fb2d1738db3eab7ccce2ce92254fc641601
0xBooper <77999320+0xBooper@users.noreply.github.com>
2021-07-22 21:31:18 -0500

    change: make it throe a error

--- src/functions/randomBool.ts
+++ src/functions/randomBool.ts
@@ -1,7 +1,7 @@
-export default function randomBool (singleDigitProbability: number) {
+export default function randomBool (singleDigitProbability: number): boolean {
     if (singleDigitProbability > 9) {
         console.error("Error: randomBool() ONLY takes a single-digit number.")
-        return new Error("Cannot use a non-single digit parameter in randomBool()")
+        throw Error("Parameter must be single digit.")
     }
 
     return Math.random() < singleDigitProbability / 10
