76d3f55abfdbc4fddc23539f4a3c4fffb83df5a5
0xBooper <77999320+0xBooper@users.noreply.github.com>
2021-07-22 21:49:41 -0500

    fix: fixed it throwing something weird

--- src/functions/randomBool.ts
+++ src/functions/randomBool.ts
@@ -1,7 +1,7 @@
 export default function randomBool (singleDigitProbability: number): boolean {
     if (singleDigitProbability > 9) {
         console.error("Error: randomBool() ONLY takes a single-digit number.")
-        throw Error("Parameter must be single digit.")
+        throw new Error("Parameter must be single digit.")
     }
 
     return Math.random() < singleDigitProbability / 10
