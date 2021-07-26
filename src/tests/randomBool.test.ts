c4e68a336d2fde13689da5687c1371368398ba1b
epikbooperia <77999320+0xBooper@users.noreply.github.com>
2021-07-25 21:03:43 -0500

    test: Make a mistake to test the Travis CI

--- src/tests/randomBool.test.ts
+++ src/tests/randomBool.test.ts
@@ -1,7 +1,7 @@
 import { randomBool } from "../index"
 
 test("randomBool's return should not be null.", () => {
-    expect(randomBool(5)).not.toBeNull()
+    expect(randomBool(5)).toBeNull()
 })
 
 test("randomBool's return should be null if randomBool is given a bad parameter", () => {
