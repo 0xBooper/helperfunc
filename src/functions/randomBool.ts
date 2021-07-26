c6c939ea099e3ecb1ce22846dea14f31c463404e
0xBooper <77999320+0xBooper@users.noreply.github.com>
2021-07-25 22:34:17 -0500

    change: Use arrow functions!

--- src/functions/randomBool.ts
+++ src/functions/randomBool.ts
@@ -1,8 +1,10 @@
-export default function randomBool (singleDigitProbability: number): boolean | null {
+const randomBool = (singleDigitProbability: number): boolean | null => {
     if (singleDigitProbability > 9) {
         console.error("Parameter should be single digit.")
         return null
     }
-
+    
     return Math.random() < singleDigitProbability / 10
 }
+
+export default randomBool
